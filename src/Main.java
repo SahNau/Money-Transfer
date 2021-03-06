b4890c878963cbe30d8e61e9b47890d4d58b8df0
Alexander Naumov <aleksnn00@gmail.com>
2021-03-06 17:14:15 +0500

    complete  cod

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[]args){
+        int count = 2000000000;
+        int price = 500000000;
+        int total = price + count;
+        System.out.println("Total:"+total);
+    }
+}
